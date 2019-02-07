b90b0f35e0d125d16d442051fb1c05fb2eb8e680
Zach Harris <zharris505@gmail.com>
2019-02-06 22:34:43 -0800

    First moving yolo tf slim model to a more flexible keras model

--- sandbox/YOLO/yolo_keras/train.py
+++ sandbox/YOLO/yolo_keras/train.py
@@ -153,6 +153,7 @@ def main():
     os.environ['CUDA_VISIBLE_DEVICES'] = cfg.GPU
 
     yolo = YOLONet()
+    exit(0)
     lp_data = lp_with_car('test')   # training on test set, which has more images....
 
     solver = Solver(yolo, lp_data)
