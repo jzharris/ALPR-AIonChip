6e03e4d9c7ca224d217b8b0b5e8f837e39e3b49d
Zach Harris <zharris505@gmail.com>
2019-02-08 09:41:03 -0800

    Successfull YOLOv2 predictions

--- sandbox/YOLOv2/keras-yolo2/utils.py
+++ sandbox/YOLOv2/keras-yolo2/utils.py
@@ -58,6 +58,13 @@ def bbox_iou(box1, box2):
 def draw_boxes(image, boxes, labels):
     image_h, image_w, _ = image.shape
 
+    # resize image for better text resolution
+    img_scale = 4
+    image = cv2.resize(image, (img_scale * image_w, img_scale * image_h))
+
+    image_w *= img_scale
+    image_h *= img_scale
+
     for box in boxes:
         xmin = int(box.xmin*image_w)
         ymin = int(box.ymin*image_h)
