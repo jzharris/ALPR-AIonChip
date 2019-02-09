99fba346ed8894c52ea552f6853721b9e5b749c8
Zach Harris <zharris505@gmail.com>
2019-02-08 19:12:51 -0800

    Pretty good contouring

--- sandbox/YOLOv2/keras-yolo2/data/lp_bb.py
+++ sandbox/YOLOv2/keras-yolo2/data/lp_bb.py
@@ -2,39 +2,76 @@ from scipy.misc import imread, toimage
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
+import copy
+from skimage import measure
+import imutils
 
 from thresholding import threshold_img
 
-fig, axes = plt.subplots(nrows=3, figsize=(7, 8))
+fig, axes = plt.subplots(nrows=4, figsize=(7, 8))
 ax = axes.ravel()
 plt.gray()
+for a in ax:
+    a.axis('off')
 
+
+##################################################################################
+# Row 1:
 # image = imread('just_lps/├╔HSB333_26.jpg', mode='L')
 image = imread('just_lps/─■A9H707.jpg', mode='L')
 ax[0].imshow(image)
 ax[0].set_title('Original')
 
+##################################################################################
+# Row 2:
 # apply thresholding
 thresh = threshold_img(image, 'global').astype(np.uint8)
 ax[1].imshow(thresh)
 ax[1].set_title('Global thresholding')
 
+##################################################################################
+# Row 3:
 # apply contouring
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cont_image = copy.deepcopy(image)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 areas = np.zeros(len(contours))
 for i, cnt in enumerate(contours):
     areas[i] = cv2.contourArea(cnt)
-
 avg_area = np.average(areas)
-for i, cnt in enumerate(contours):
-    if areas[i] > avg_area:
-        cv2.drawContours(image, [cnt], 0, (255, 255, 255), 3)
 
-ax[2].imshow(image)
+# remove extremely large contours
+max_tolerance = 0.8
+min_tolerance = 0.2
+filtered_contours = []
+for i, cnt in enumerate(contours):
+    if avg_area - (avg_area - np.min(areas)) * min_tolerance < areas[i] < (np.max(areas) - avg_area) * max_tolerance + avg_area:
+        cv2.drawContours(cont_image, [cnt], 0, (255, 255, 255), 3)
+        filtered_contours.append(cnt)
+
+ax[2].imshow(cont_image)
 ax[2].set_title('Contoured')
 
-for a in ax:
-    a.axis('off')
+##################################################################################
+# Row 4:
+charCandidates = np.zeros(thresh.shape, dtype="uint8")
+
+c = max(filtered_contours, key=cv2.contourArea)
+for i, cnt in enumerate(filtered_contours):
+    (boxX, boxY, boxW, boxH) = cv2.boundingRect(cnt)
+    aspectRatio = boxW / float(boxH)
+    solidity = cv2.contourArea(c) / float(boxW * boxH)
+    heightRatio = boxH / float(image.shape[0])
+
+    keepAspectRatio = aspectRatio < 1.0
+    keepSolidity = solidity > 0.15
+    keepHeight = 0.4 < heightRatio < 0.95
+
+    if keepAspectRatio and keepSolidity and keepHeight:
+        hull = cv2.convexHull(cnt)
+        cv2.drawContours(charCandidates, [hull], -1, 255, -1)
+
+ax[3].imshow(charCandidates)
+ax[3].set_title('Candidates')
 
 plt.show()
