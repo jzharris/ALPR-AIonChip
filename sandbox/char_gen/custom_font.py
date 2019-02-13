e08916e666f054b752d7c0bf1bdb5624501eeb0a
Zach Harris <zharris505@gmail.com>
2019-02-13 11:23:23 -0800

    Adding character generator

---
+++ sandbox/char_gen/custom_font.py
@@ -0,0 +1,30 @@
+import numpy as np
+from PIL import ImageFont, ImageDraw, Image
+import cv2
+import time
+
+## Make canvas and set the color
+img_width = 475
+img_height = 207
+img = np.zeros((img_height, img_width, 3), np.uint8)
+b, g, r, a = 0, 0, 0, 1
+
+# set the rectangle background to white
+rectangle_bgr = (255, 255, 255)
+cv2.rectangle(img, (0, 0), (img_width, img_height), rectangle_bgr, cv2.FILLED)
+
+## Use simsum.ttc to write Chinese.
+fontpath = "./LICENSE_PLATE_USA.ttf"
+font = ImageFont.truetype(fontpath, 125)
+
+img_pil = Image.fromarray(img)
+draw = ImageDraw.Draw(img_pil)
+draw.text((30, 30), "A 12345", font=font, fill=(b, g, r, a))
+img = np.array(img_pil)
+
+## Display
+cv2.imshow("res", img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+cv2.imwrite("res.png", img)
