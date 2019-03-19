ca558aea18841259ff32ed6b47c0dffc91251faa
Zach Harris <zharris505@gmail.com>
2019-03-19 14:18:20 -0700

    Adding end-to-end test 2

--- main/LP_segmentation/predict.py
+++ main/LP_segmentation/predict.py
@@ -2,19 +2,21 @@
 
 import argparse
 import os
+import shutil
 import cv2
 import numpy as np
 from tqdm import tqdm
 from preprocessing import parse_annotation
-from utils import draw_boxes
+from utils import draw_boxes, crop_image
 from frontend import YOLO
 import json
+import xml.etree.ElementTree
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 ########################################################################################################################
-# run: python predict.py -c config_lp_seg_mobilenet.json -i predictions\AC_3.jpg
+# run: python predict.py -c config_lp_seg_mobilenet_quant.json
 ########################################################################################################################
 
 argparser = argparse.ArgumentParser(
@@ -25,19 +27,36 @@ argparser.add_argument(
     '--conf',
     help='path to configuration file')
 
-argparser.add_argument(
-    '-i',
-    '--input',
-    help='path to an image or an video (mp4 format)')
+
+# Convert XML to nested dictionary
+def xml_to_dict(path):
+    def xml_to_dict_recursive(node):
+        if len(node)==0:
+            return node.text
+        return {child.tag:xml_to_dict_recursive(child) for child in node}
+    return xml_to_dict_recursive(xml.etree.ElementTree.parse(path).getroot())
+
 
 def _main_(args):
     config_path  = args.conf
-    image_path   = args.input
-
-    with open(config_path) as config_buffer:    
+    with open(config_path) as config_buffer:
         config = json.load(config_buffer)
 
-    weights_path = config['train']['pretrained_weights']
+    image_folder = config['predict']['input']
+    output_folder = config['predict']['output_folder']
+    crop = config['predict']['crop']
+
+    if crop:
+        target_folder = "{}_cropped".format(output_folder)
+    else:
+        target_folder = "{}_detected".format(output_folder)
+
+    detected_folder = os.path.join(image_folder, target_folder)
+    if os.path.exists(detected_folder):
+        shutil.rmtree(detected_folder)
+    os.mkdir(detected_folder)
+
+    weights_path = config['predict']['pretrained_weights']
 
     ###############################
     #   Make the model 
@@ -59,44 +78,72 @@ def _main_(args):
     #   Predict bounding boxes 
     ###############################
 
-    if image_path[-4:] == '.mp4':
-        video_out = image_path[:-4] + '_detected' + image_path[-4:]
-        video_reader = cv2.VideoCapture(image_path)
+    # dictionary of exported images used to generate sample.txt file:
+    samples_dict = {}
 
-        nb_frames = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
-        frame_h = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        frame_w = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
+    for root, dirs, files in os.walk(os.path.join(image_folder, "jpeg")):
+        for file in tqdm(files):
+            image_path = os.path.join(root, file)
+            detected_path = os.path.join(detected_folder, file)
 
-        video_writer = cv2.VideoWriter(video_out,
-                               cv2.VideoWriter_fourcc(*'MPEG'), 
-                               50.0, 
-                               (frame_w, frame_h))
+            if image_path[-4:] == '.mp4':
+                video_out = image_path[:-4] + '_detected' + image_path[-4:]
+                video_reader = cv2.VideoCapture(image_path)
 
-        for i in tqdm(range(nb_frames)):
-            _, image = video_reader.read()
-            
-            boxes = yolo.predict(image)
-            image = draw_boxes(image, boxes, config['model']['labels'])
+                nb_frames = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
+                frame_h = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                frame_w = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
 
-            video_writer.write(np.uint8(image))
+                video_writer = cv2.VideoWriter(video_out,
+                                       cv2.VideoWriter_fourcc(*'MPEG'),
+                                       50.0,
+                                       (frame_w, frame_h))
 
-        video_reader.release()
-        video_writer.release()  
-    else:
-        image = cv2.imread(image_path)
-        boxes = yolo.predict(image)
+                for i in tqdm(range(nb_frames)):
+                    _, image = video_reader.read()
 
-        best_box = []
-        for box in boxes:
-            if len(best_box) == 0:
-                best_box.append(box)
-            elif box.get_score() > best_box[0].get_score():
-                best_box[0] = box
-        image = draw_boxes(image, best_box, config['model']['labels'])
+                    boxes = yolo.predict(image)
+                    image = draw_boxes(image, boxes, config['model']['labels'])
 
-        print(len(best_box), 'boxes are chosen')
+                    video_writer.write(np.uint8(image))
 
-        cv2.imwrite(image_path[:-4] + '_detected' + image_path[-4:], image)
+                video_reader.release()
+                video_writer.release()
+            else:
+                image = cv2.imread(image_path)
+                boxes = yolo.predict(image)
+
+                if crop:
+                    best_box = []
+                    for box in boxes:
+                        if len(best_box) == 0:
+                            best_box.append(box)
+                        elif box.get_score() > best_box[0].get_score():
+                            best_box[0] = box
+
+                    if len(best_box) == 0:
+                        print('no boxes are chosen')
+
+                    cropped_image = crop_image(image, best_box[0])
+
+                    cv2.imwrite(detected_path[:-4] + '' + detected_path[-4:], cropped_image)
+
+                    # find LP chars to add to sample.txt file
+                    xml = xml_to_dict(os.path.join(image_folder, "xml", "{}.xml".format(file[:-4])))
+                    samples_dict[file] = xml['object']['platetext']
+                else:
+                    image = draw_boxes(image, boxes, config['model']['labels'])
+                    if len(boxes) == 0:
+                        print('no boxes are chosen')
+
+                    cv2.imwrite(detected_path[:-4] + '' + detected_path[-4:], image)
+
+    if crop:
+        # write sample.txt file:
+        print('writing sample.txt file to {}'.format(os.path.join(detected_folder, 'sample.txt')))
+        with open(os.path.join(detected_folder, 'sample.txt'), 'w+') as samples_txt:
+            for key in samples_dict.keys():
+                samples_txt.write("{} {}\n".format(key, samples_dict[key]))
 
 if __name__ == '__main__':
     args = argparser.parse_args()
