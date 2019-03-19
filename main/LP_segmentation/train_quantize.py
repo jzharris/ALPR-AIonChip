1acfd2fa68f776f9b996aad67c70011e2cef8572
Zach Harris <zharris505@gmail.com>
2019-03-18 18:00:38 -0700

    Removing old code

--- main/LP_segmentation/train_quantize.py
+++ main/LP_segmentation/train_quantize.py
@@ -23,26 +23,6 @@ white_regex = ['CustomAdam', 'loss', 'training',
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
-# one method to do quantization
-def strategy_1(v_numpy):
-    v_numpy_shape = v_numpy.shape
-    v_numpy_flatten = v_numpy.flatten()
-    min_np = v_numpy.min()
-    max_np = v_numpy.max()
-    gap = (max_np - min_np) / (NUM_OF_LEVEL - 1)
-    levels = np.asarray([(min_np + x * gap) for x in range(NUM_OF_LEVEL)])
-    for i in range(len(v_numpy_flatten)):
-        v_numpy_flatten[i] = min(levels, key=lambda x : abs(x-v_numpy_flatten[i]))
-    v_numpy = v_numpy_flatten.reshape(v_numpy_shape)
-    return v_numpy
-
-
-def quantize_one_variable(sess, v):
-    v_numpy = sess.run(v)
-    new_v = strategy_1(v_numpy)
-    assign_op = v.assign(new_v)
-    return assign_op
-
 argparser = argparse.ArgumentParser(
     description='Train and validate YOLO_v2 model on any dataset')
 
